2db6051d08dd34be7f5340afa59f7e143e68c923
ritvikraj115 <143361673+ritvikraj115@users.noreply.github.com>
2024-06-13 10:46:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask,request,jsonify
 import requests
 import os
+import logging
 
 
 app= Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def index():
+    logging.basicConfig(level=logging.INFO)
     data= request.get_json()
-    print(data)
+    app.logger.info(data)
     source_currency= data['queryResult']['parameters']['unit-currency']['currency']
     amount=data['queryResult']['parameters']['unit-currency']['amount']
     target_currency=data['queryResult']['parameters']['currency-name']
@@ -27,11 +29,11 @@ def fetch_conversion_factor(source,target):
     url="https://free.currconv.com/api/v7/convert?q={}_{}&compact=ultra&apiKey=9f180e999b75dab652a5".format(source,target)
     response = requests.get(url)
     response= response.json()
-    print(response)
+    app.logger.info(response)
 
     return response["{}_{}".format(source,target)]
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
-    print(f"Starting Flask app on port {port}")
+   app.logger.info(f"Starting Flask app on port {port}")
     app.run(host='0.0.0.0', port=port)
