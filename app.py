571a7ce148ab29fc9fb98b4af7b6628d1601e27a
ritvikraj115 <143361673+ritvikraj115@users.noreply.github.com>
2024-06-13 09:52:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,5 +30,6 @@ def fetch_conversion_factor(source,target):
 
     return response['{}_{}'.format(source,target)]
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port)
